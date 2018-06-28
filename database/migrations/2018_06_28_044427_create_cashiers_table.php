c27e7ccc51ccdb1236af2151a7befc81b7de78aa
dulajdilshan <balasuriyadulaj@gmail.com>
2018-06-28 12:51:04 +0530

    cashier, manager migrations were made - have to complete database

---
+++ database/migrations/2018_06_28_044427_create_cashiers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCashiersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cashiers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('nic');
+            $table->string('email')->unique();
+            $table->string('full_name');
+            $table->text('address');
+            $table->string('contact_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cashiers');
+    }
+}
