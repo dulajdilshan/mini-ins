612339fbaaaba4f1f5294c67f86595fa2546d7e2
dulajdilshan <balasuriyadulaj@gmail.com>
2018-06-29 18:49:26 +0530

    payments migration

---
+++ database/migrations/2018_06_29_131619_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('loan_id');
+            $table->unsignedInteger('customer_id');
+            $table->bigInteger('amount');
+            $table->unsignedInteger('week_no');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
