a5b15813e87cdfb932b686e633ece83f54e34fd4
dulajdilshan <balasuriyadulaj@gmail.com>
2018-06-29 19:45:15 +0530

    users , manager, cashier seeders done

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,12 +11,69 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
+//         $this->call(UsersTableSeeder::class);
 
 //        $this->call([
 //            UsersTableSeeder::class,
 //            PostsTableSeeder::class,
 //            CommentsTableSeeder::class,
 //        ]);
+
+        //Users
+        DB::table('users')->insert([
+            'id' => '1',
+            'name' => 'Super Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '2',
+            'name' => 'Super Manager',
+            'email' => 'manager@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '3',
+            'name' => 'Super Cashier',
+            'email' => 'cashier@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '4',
+            'name' => 'Super User',
+            'email' => 'user@mail.com',
+            'password' => bcrypt('password'),
+        ]);
+
+        //Others
+
+        DB::table('admins')->insert([
+            'user_id' => '1',
+            'email' => 'admin@mail.com',
+        ]);
+
+        DB::table('managers')->insert([
+            'nic' => '21',
+            'user_id' => '2',
+            'email' => 'manager@mail.com',
+            'full_name' => 'Super Manager',
+            'address' => '33',
+            'contact_no' => '12',
+        ]);
+
+        DB::table('cashiers')->insert([
+            'nic' => '34',
+            'user_id' => '3',
+            'email' => 'cashier@mail.com',
+            'full_name' => 'Super Cashier',
+            'address' => '33',
+            'contact_no' => '12',
+        ]);
+
+
+
     }
 }
